9298ce652dcbd56cf54194845d270e64555cad02
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-07 10:22:57 -0500

    Update Q55.java

--- src/pack1/Q55.java
+++ src/pack1/Q55.java
@@ -1,11 +1,11 @@
-package nov11;
+package pack1;
 
 public class Q55 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] num = {1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 8};
+        int[] num = {1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 8};
 
         for (int i = 0; i < num.length; i++) {
             for (int j = i + 1; j < num.length; j++) {
@@ -14,8 +14,6 @@ public class Q55 {
                 }
             }
         }
-	}
-		
 	}
 
 }
