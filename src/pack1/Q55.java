e381e08be98e63d44e55aff6ba5fc03f7745fd3c
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-07 10:20:47 -0500

    Update Q55.java

--- src/pack1/Q55.java
+++ src/pack1/Q55.java
@@ -5,19 +5,16 @@ public class Q55 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr = { 520, 59, 65, 100, 523, 59 };
-				
-		System.out.println();
-		
-		int samenumber = 0;
-		for (int i = 1; i<arr.length; i++) {
-			samenumber = arr[i];
-			for (int j = 1; j<arr.length; j++) {
-				if (arr[i] == samenumber) {
-					System.out.print(samenumber);
-				} 
-			}
-		}
+		int[] num = {1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 8};
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                if (num[i] == num[j]) {
+                    System.out.println("Duplicate value found: " + num[i]);
+                }
+            }
+        }
+	}
 		
 	}
 
