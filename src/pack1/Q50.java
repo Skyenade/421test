2c8e2c6d3659b7fcbd1c890a3badbbb34116ca2f
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-10 22:28:00 +0000

    trying to commit on tablet

--- src/pack1/Q50.java
+++ src/pack1/Q50.java
@@ -5,15 +5,17 @@ public class Q50 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String[] arr = { "Rio", "Sao Paulo", "Brasilia", "Belo Horizonte" };
+		// String[] arr = { "Rio", "Sao Paulo", "Brasilia", "Belo Horizonte" };
 
-		if (arr[0] == "Rio") {
-			arr[0] = null;
-		}
+		// if (arr[0] == "Rio") {
+		// 	arr[0] = null;
+		// }
 
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
+		// for (int i = 0; i < arr.length; i++) {
+		// 	System.out.print(arr[i] + " ");
+		// }
+
+		
 
 	}
 
---
+++ src/pack1/Q72.java
@@ -0,0 +1,8 @@
+package pack1;
+
+public class Q72{
+    public static void main(String[] args){
+        int i = 0;
+        System.out.println(i);
+    } 
+}
