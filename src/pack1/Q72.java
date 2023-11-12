a1928a2a085b942353b15ab76b4a8441186cb706
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-12 23:56:38 +0000

    sunday commit

--- src/pack1/Q72.java
+++ src/pack1/Q72.java
@@ -2,7 +2,26 @@ package pack1;
 
 public class Q72{
     public static void main(String[] args){
-        int i = 0;
-        System.out.println(i);
+        int[] array = {20, 20, 30, 40, 50, 50, 50};
+        int newLength = removeDuplicates(array);
+        
+        System.out.println("New length of the array: " + newLength);
+    }
+
+    public static int removeDuplicates(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+
+        int uniqueIndex = 0;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[uniqueIndex]) {
+                uniqueIndex++;
+                array[uniqueIndex] = array[i];
+            }
+        }
+
+        return uniqueIndex + 1;
     } 
 }
