210402084bf78d69e3c0b4aa0674188d24e1b4ed
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-14 11:49:49 -0500

    Create Q85.java

---
+++ src/pack1/Q85.java
@@ -0,0 +1,28 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class Q85 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		int num = kb.nextInt();
+		
+		System.out.println("Input number is " + num);
+		
+		cube(num);		
+	}
+	
+	public static void cube(int num) {
+				
+		for (int i = 1; i<=num; i++) {
+			int cubic = i * i * i;
+			System.out.println("Number is " + i + " and cube of " + i + " is " + cubic);
+		}
+		
+	}
+
+}
