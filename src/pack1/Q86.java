a5c496b3f99c642ad1d1ac4844f86bc26e2f43f4
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-14 13:27:27 -0500

    Create Q86.java

---
+++ src/pack1/Q86.java
@@ -0,0 +1,29 @@
+package pack1;
+
+import java.util.Scanner;
+
+public class Q86 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number: ");
+		int num = kb.nextInt();
+		kb.close();
+		
+		
+		
+	}
+	
+	public static int seriesSum(int num) {
+		int sum = 0;
+		
+		for (int i = 0; i<=num; i++) {
+			
+		}
+		
+		return sum;
+	}
+
+}
