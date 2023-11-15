83db3c339d83449c13615ab1bf55aa7502505c90
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 14:03:35 -0500

    Update Q57.java

--- src/pack1/Q57.java
+++ src/pack1/Q57.java
@@ -2,6 +2,13 @@ package pack1;
 
 public class Q57{
     public static void main(String[] args ){
-        
+        String [] ar1 = {"Rio","Brasilia","SP"};
+		String [] ar2 = {"Floripa","PA","SP"};
+		
+		for (int i = 0; i<ar1.length;i++) {
+			if(ar1[i] == ar2[i]) {
+				System.out.println("Duplicate found: " + ar1[i] + " in index " + i); 
+			}
+		}
     }
 }
