97a24b974b9c306d723423433d27ea69603e673f
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 13:55:51 -0500

    Update Q22.java

--- src/pack1/Q22.java
+++ src/pack1/Q22.java
@@ -27,23 +27,23 @@ public class Q22 {
 			System.out.println("You don't have a root");
 		}
 		
-//		myquadmethod(a,b,c);
+		myquadmethod(a,b,c);
 	}
 	
-//	public static void myquadmethod(double a, double b, double c) {
-//		double d = (b*b) - 4*a*c;
-//		if (d >0) {
-//			double root1 = (-b + Math.sqrt(d))/(2*a);
-//			double root2 = (-b - Math.sqrt(d))/(2*a);
-//			System.out.println("Root 1 is " + root1);
-//			System.out.println("Root 2 is " + root2);
-//		} else if (d ==0){
-//			double roots = -b/(2*a);
-//			System.out.println("Root 1 is " + roots);
-//		} else {
-//			System.out.println("You don't have a root");
-//		}
-//		
-//	}
+	public static void myquadmethod(double a, double b, double c) {
+		double d = (b*b) - 4*a*c;
+		if (d >0) {
+			double root1 = (-b + Math.sqrt(d))/(2*a);
+			double root2 = (-b - Math.sqrt(d))/(2*a);
+			System.out.println("Root 1 is " + root1);
+			System.out.println("Root 2 is " + root2);
+		} else if (d ==0){
+			double roots = -b/(2*a);
+			System.out.println("Root 1 is " + roots);
+		} else {
+			System.out.println("You don't have a root");
+		}
+		
+	}
 
 }
