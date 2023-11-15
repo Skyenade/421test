d2b3b41a90c5ebf4bc129b61012e9248dd2018bd
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 14:01:44 -0500

    Update Q50.java

--- src/pack1/Q50.java
+++ src/pack1/Q50.java
@@ -5,17 +5,36 @@ public class Q50 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// String[] arr = { "Rio", "Sao Paulo", "Brasilia", "Belo Horizonte" };
+		int[] MyArray = { 10, 20, 30, 40, 50 };
 
-		// if (arr[0] == "Rio") {
-		// 	arr[0] = null;
-		// }
+		int[] newArray = null;
+		int index = -1;
+		int remove = 30;
 
-		// for (int i = 0; i < arr.length; i++) {
-		// 	System.out.print(arr[i] + " ");
-		// }
+		for (int i = 0; i < MyArray.length; i++) {
+			if (MyArray[i] == remove) {
+				index = i;
+				break;
+			}
+		}
 
-		
+		while (index != -1) {
+			newArray = new int[MyArray.length - 1];
+			int newIndex = 0;
+			for (int i = 0; i < MyArray.length; i++) {
+				if (i != index) {
+					newArray[newIndex] = MyArray[i];
+					newIndex++;
+				}			}
+		}
+
+		for (int i = 0; i < MyArray.length; i++) {
+			System.out.println("1st array " + MyArray[i]);
+		}
+
+		for (int i = 0; i < newArray.length; i++) {
+			System.out.println("2nd array " + newArray[i]);
+		}		
 
 	}
 
