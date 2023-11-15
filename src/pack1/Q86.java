f0e154d0ae29214e7b06ec477bcdadf52a7499ad
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 13:13:21 -0500

    Update Q86.java

--- src/pack1/Q86.java
+++ src/pack1/Q86.java
@@ -6,21 +6,23 @@ public class Q86 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner kb = new Scanner(System.in);
 		System.out.println("Please enter a number: ");
 		int num = kb.nextInt();
 		kb.close();
 		
-		
+		int add = seriesSum(num);
+		System.out.println(add);
 		
 	}
 	
 	public static int seriesSum(int num) {
 		int sum = 0;
+		int currentnumber = 0;
 		
-		for (int i = 0; i<=num; i++) {
-			
+		for (int i = 0; i<num; i++) {
+			currentnumber = currentnumber *10 +1;
+			sum += currentnumber;
 		}
 		
 		return sum;
