84bd32951b4e5ef7e0066e0fadb8bab6cd2f4317
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 14:57:39 -0500

    Update Q90g.java

--- src/pack1/Q90g.java
+++ src/pack1/Q90g.java
@@ -12,46 +12,30 @@ public class Q90g {
 		int n = kb.nextInt();
 		kb.close();
 		
-		for (int i = 1; i <= n; i++) {
-            // Print spaces before the numbers
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            // Print decreasing numbers
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
-            }
-
-            // Print increasing numbers excluding the first
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
-            }
-
-            // Move to the next line after each row
-            System.out.println();
-        }
-
-        // Print lower part of the pattern
-        for (int i = n - 1; i >= 1; i--) {
-            // Print spaces before the numbers
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-
-            // Print decreasing numbers
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
-            }
-
-            // Print increasing numbers excluding the first
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
-            }
-
-            // Move to the next line after each row
-            System.out.println();
-        }
+		for (int i = 1; i <= n; i++) {	            
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }	            
+	            for (int j = i; j >= 1; j--) {
+	                System.out.print(j);
+	            }	            
+	            for (int j = 2; j <= i; j++) {
+	                System.out.print(j);
+		    }
+	            System.out.println();
+	        }	
+	        for (int i = n - 1; i >= 1; i--) {	            
+	            for (int j = 1; j <= n - i; j++) {
+	                System.out.print(" ");
+	            }	
+	            for (int j = i; j >= 1; j--) {
+	                System.out.print(j);
+	            }	
+	            for (int j = 2; j <= i; j++) {
+	                System.out.print(j);
+		    }	
+	            System.out.println();
+	        }
 	}
 
 }
