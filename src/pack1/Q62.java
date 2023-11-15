2abcb260846890251df4f37aad2d265abe3c1e75
Maina Canhini <68037583+Skyenade@users.noreply.github.com>
2023-11-15 14:25:59 -0500

    Update Q62.java

--- src/pack1/Q62.java
+++ src/pack1/Q62.java
@@ -5,27 +5,22 @@ import java.util.Scanner;
 public class Q62{
 	public static void main(String[] args){
 		
-		Scanner scanner = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
 
-        // Input matrix dimensions
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
 
         System.out.print("Enter the number of columns: ");
         int columns = scanner.nextInt();
 
-        // Input first matrix
         System.out.println("Enter elements for the first matrix:");
         int[][] matrix1 = readMatrix(rows, columns, scanner);
 
-        // Input second matrix
         System.out.println("Enter elements for the second matrix:");
         int[][] matrix2 = readMatrix(rows, columns, scanner);
 
-        // Add matrices
         int[][] sumMatrix = addMatrices(matrix1, matrix2);
 
-        // Display result
         System.out.println("Sum of the matrices:");
         printMatrix(sumMatrix);
 
@@ -46,7 +41,6 @@ public class Q62{
         return matrix;
     }
 
-    // Function to add two matrices
     private static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
         int rows = matrix1.length;
         int columns = matrix1[0].length;
